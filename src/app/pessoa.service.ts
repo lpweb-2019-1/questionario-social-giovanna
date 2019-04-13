28e6779693c756a63708aab57c089375668edda3
GIOVANNA BIAGI FILIPAKIS SOUZA <34981010@labin.canoas>
2019-04-13 11:58:45 -0300

    Falta resolver o problema do storage

--- src/app/pessoa.service.ts
+++ src/app/pessoa.service.ts
@@ -7,6 +7,7 @@ import {AppComponent} from './app.component';
 export class PessoaService {
 pessoas = [];
   constructor() {this.pessoas = []}
+  
   salvar(nome: string, idade: number, sexo: string, cidade: string) {
     const pessoa = {
       nome,
